399f3cbf5d19edf24344aa9bc57df1322a694152
PVV <vladislav304304@gmail.com>
2020-01-03 18:51:14 +0200

    add solution the clock 3

---
+++ tutor_tasks/best/the_clock_3.py
@@ -0,0 +1,15 @@
+# С начала суток часовая стрелка повернулась
+# на угол в α градусов. Определите сколько полных
+# часов, минут и секунд прошло с начала суток,
+# то есть решите задачу, обратную задаче «Часы - 1». Запишите ответ в три переменные и выведите их на экран.
+
+h_angle = float(input())
+
+total_hours = h_angle / 30
+total_seconds = total_hours * 3600  # h_angle*120
+hours = int(total_seconds // 3600)
+left_from_hours = total_seconds % 3600
+minutes = int(left_from_hours // 60)
+seconds = int(left_from_hours % 60)
+
+print(hours, minutes, seconds)
