60e87994441150abece16c470fa7458433eb2eec
PVV <vladislav304304@gmail.com>
2020-01-06 16:28:13 +0200

    updates task delete_fragment

--- tutor_tasks/delete_fragment.py
+++ tutor_tasks/delete_fragment.py
@@ -6,3 +6,11 @@ s = input()
 ind_1 = s.find('h')
 ind_2 = s.rfind('h')
 print(s[:ind_1] + s[ind_2 + 1:])
+
+# solution of developers (is not better than mine):
+
+s = input()
+part_1 = s[:s.find('h')]
+part_2 = s[s.rfind('h') + 1:]
+
+print(part_1 + part_2)
